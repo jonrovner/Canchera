68045085798429c5eb0cd80b5b36773c71b02de7
jonrovner <jonrovner@gmail.com>
2022-03-10 17:35:34 -0300

    seteando estructura de proyecto

---
+++ src/components/clubDetail/ClubDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Clubdetail = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Clubdetail;
---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Canchera</h1>
+
+        </div>
+    );
+}
+
+export default Landing;
