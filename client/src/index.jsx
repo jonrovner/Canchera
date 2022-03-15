442dbdd2d7115d3b2604badfbd3a8c566b491f06
JavierMusso <javier.musso96@gmail.com>
2022-03-15 18:42:24 -0300

    agrega provider y router a index

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
