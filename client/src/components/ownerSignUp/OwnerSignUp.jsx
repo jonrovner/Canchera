bf4e2c6b1a1b31ae84e14b0f390cb449960c7edb
Nico Yañez <airtonicolasy1996@gmail.com>
2022-03-15 19:42:17 -0300

    Formulario controlado de registro de usuario host terminado en funcionalidad

--- client/src/components/ownerSignUp/OwnerSignUp.jsx
+++ client/src/components/ownerSignUp/OwnerSignUp.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import style from "./OwnerSignUp.module.scss";
+import { Validate } from "./validaciones/validaciones.js";
 
-const Ownersignup = () => {
-  return <div>Owner Sign up</div>;
+const OwnerSignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const handlerInputChange = (e) => {
+    var value = e.target.value;
+    var name = e.target.name;
+    setData((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      };
+    });
+    setError(Validate({ ...data, [name]: value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("enviado");
+    /* dispatch(); */
+  };
+
+  /* const disabeledSubmit = useMemo(() => {
+    if (error.name || error.email || error.password) {
+      return true;
+    }
+
+    return false;
+  }, [error]); */
+
+  return (
+    <div className={style.contenedor}>
+      <form
+        onSubmit={(e) => {
+          onSubmit(e);
+        }}
+      >
+        <h1>Registro</h1>
+        <div>
+          <input
+            autoComplete="off"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            onChange={(e) => handlerInputChange(e)}
+          />
+          {error.name && <p>{error.name}</p>}
+        </div>
+        <div>
+          <input
+            autoComplete="off"
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={(e) => handlerInputChange(e)}
+          />
+          {error.email && <p>{error.email}</p>}
+        </div>
+        <div>
+          <input
+            autoComplete="off"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            onChange={(e) => handlerInputChange(e)}
+          />
+          {error.password && <p>{error.password}</p>}
+        </div>
+        <div>
+          <input
+            type="submit"
+            className={style.boton}
+            /* disabled={disabeledSubmit} */
+            value="Send"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default Ownersignup;
+export default OwnerSignUp;
