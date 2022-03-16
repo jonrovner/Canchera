70f84c4231580a075cacf60be4be85f3f96b98f4
LucasZupan <lezupan@hotmail.com>
2022-03-16 14:02:13 -0300

    Rutas para traer canchas agregadas

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,18 +1,21 @@
 import {Router, Request, Response} from "express"
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 //const axios = require('axios');
-// const PokemonRoutes = require('./pokemonRoutes');
+
  const SignUp = require('./signup');
  const User = require('./user')
+ const Field = require('./field')
 
 const router = Router();
 
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
  router.use('/', SignUp);
  router.use('/', User)
-// router.use('/types', TypeRoutes);
+ router.use('/', Field)
+
 
 module.exports = router;
