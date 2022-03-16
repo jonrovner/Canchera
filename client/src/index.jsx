ed165d8c79ef8052c49ad32fd1b202a725097110
Airton Nicolas Yañez <83186353+NicoYanez9621@users.noreply.github.com>
2022-03-15 21:23:09 -0300

    Host create formulario usuario (#12)
    * Formulario controlado de registro de usuario host terminado en funcionalidad
    
    * cree carpeta de redux/ agregue formulario controlado con modal/ faltaria hacer action para consumir de base de datos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import store from "./store";
+import store from "./store/Index";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
