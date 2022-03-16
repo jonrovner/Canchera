5c1e507079821df4f8c73e18948205ca35772139
JavierMusso <javier.musso96@gmail.com>
2022-03-15 23:36:43 -0300

    small typos at index filenames

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import store from "./store/Index";
+import store from "./redux/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
