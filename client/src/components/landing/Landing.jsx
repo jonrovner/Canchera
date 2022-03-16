cf321c2dbc9496d6a74546c07ae3328c38714f29
JavierMusso <javier.musso96@gmail.com>
2022-03-15 21:17:32 -0300

    adds css reset to index. Footer and Slider components created.

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import styles from "./Landing.module.css";
+import NavBar from "../NavBar/NavBar";
+import Slider from "../Slider/Slider";
+import Footer from "../Footer/Footer";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Canchera</h1>
-      <h2>Hola muchaches</h2>
-      <h2>Otro cambio realizado en rama "Cree carpeta"</h2>
+    <div className={styles.Landing}>
+      <NavBar />
+      <section>Hero section</section>
+      <Slider />
+      <section>Jugador section</section>
+      <section>fotos varias + info</section>
+      <section>host section</section>
+      <Footer />
     </div>
   );
 };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
+import "./reset.css";
 
 ReactDOM.render(
   <React.StrictMode>
