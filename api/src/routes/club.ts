4f43641ef4c5615896a38902cd0a25a6b0f237f8
leo10-kz <88455824+leo10-kz@users.noreply.github.com>
2022-03-17 19:31:03 +0100

    Listo el get a detail (#43)

--- api/src/routes/club.ts
+++ api/src/routes/club.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 const router = Router();
 const ClubController = require('../controller/ClubController')
 
-router.post('/createdclub', ClubController.postClub);
-router.get('/clubs', ClubController.getClubs);
-router.get('/:clubId')
+router.post('/club', ClubController.postClub);
+router.get('/club', ClubController.getClubs);
+router.get('/club/:id', ClubController.clubDetail)
 
 
 module.exports = router;
