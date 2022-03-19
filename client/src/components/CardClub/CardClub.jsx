280d2c54315b3af3e8972478f0598f3768ef0dc7
Airton Nicolas Yañez <83186353+NicoYanez9621@users.noreply.github.com>
2022-03-19 17:21:30 -0300

    Arregle duplicado archivos (#74)
    * Configuracion de back
    
    * arregle codigo que se duplico con el anterior merge

--- client/src/components/CardClub/CardClub.jsx
+++ client/src/components/CardClub/CardClub.jsx
@@ -21,27 +21,6 @@ const CardClub = ({ id, name, img, location, openHour, closeHour }) => {
         </div>
       </div>
     </Link>
-
-import style from "./CardClub.module.scss";
-
-const CardClub = ({ name, img, location, openHour, closeHour }) => {
-  console.log("name: ", name);
-  return (
-    <div className={style.contenedor}>
-      <div className={style.divImg}>
-        <img className={style.img} src={img} alt="not found" />
-      </div>
-      <div className={style.divInfo}>
-        <h2>{name}</h2>
-        <div className={style.divContenido}>
-          <span>{location}</span>
-          <span>
-            Horario: {openHour}am-{closeHour}pm
-          </span>
-        </div>
-      </div>
-    </div>
-
   );
 };
 
