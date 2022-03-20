11e8fc8ebbeae370a9a177a6d2262c5a261bf4c9
JavierMusso <javier.musso96@gmail.com>
2022-03-20 20:19:31 -0300

    adds lowestPrice to each club. where it stores the lowest field price, to filter clubs by price

--- api/src/models/club.ts
+++ api/src/models/club.ts
@@ -1,5 +1,5 @@
-'use strict';
-import { Model, UUIDV4 } from 'sequelize';
+"use strict";
+import { Model, UUIDV4 } from "sequelize";
 
 interface ClubAttributes {
   id: number;
@@ -12,12 +12,11 @@ interface ClubAttributes {
   score: string;
   latitude: number;
   longitude: number;
+  lowestPrice: number;
 }
 
-
 module.exports = (sequelize: any, DataTypes: any) => {
-  class Club extends Model<ClubAttributes>
-  implements ClubAttributes {
+  class Club extends Model<ClubAttributes> implements ClubAttributes {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -30,66 +29,71 @@ module.exports = (sequelize: any, DataTypes: any) => {
     openHour!: number;
     closeHour!: number;
     image!: string;
-    score!: string; 
+    score!: string;
     latitude!: number;
     longitude!: number;
+    lowestPrice!: number;
 
     static associate(models: any) {
       // define association here
       Club.belongsTo(models.User);
-      Club.hasMany(models.Field)
-      
+      Club.hasMany(models.Field);
     }
   }
-  Club.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {              
-        len: {
-          args: [2, 30],
-          msg: "El nombre debe tener mas de 2 caracteres"
+  Club.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [2, 30],
+            msg: "El nombre debe tener mas de 2 caracteres",
+          },
         },
-      }
-    },    
-    description: {
-      type: DataTypes.STRING(1500)      
-      },        
+      },
+      description: {
+        type: DataTypes.STRING(1500),
+      },
       location: {
         type: DataTypes.STRING,
-        allowNull: false
-      },    
-    openHour: {
-      type: DataTypes.INTEGER,
-      allowNull: false 
+        allowNull: false,
       },
-    closeHour: {
-      type: DataTypes.INTEGER,
-      allowNull: false 
-      },    
-    image: {
-      type: DataTypes.STRING
+      openHour: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      closeHour: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      score: {
+        type: DataTypes.ENUM,
+        values: ["1", "2", "3", "4", "5"],
+      },
+      latitude: {
+        type: DataTypes.FLOAT,
+      },
+      longitude: {
+        type: DataTypes.FLOAT,
+      },
+      lowestPrice: {
+        type: DataTypes.INTEGER,
+      },
     },
-    score: {
-      type: DataTypes.ENUM,
-      values: ['1', '2', '3', '4', '5']      
-    },
-    latitude: {
-      type: DataTypes.FLOAT
-    },
-    longitude: {
-      type: DataTypes.FLOAT
-    },
-   
-  }, {
-    sequelize,
-    modelName: 'Club',
-  });
+    {
+      sequelize,
+      modelName: "Club",
+    }
+  );
   return Club;
 };
