e1d6f469c3b36fe74bbef34361065a9c60d957e2
Javier Musso <javier.musso96@gmail.com>
2022-03-20 19:05:37 -0300

    fixea algunos errores menores en la landing. gracias Eze! (#85)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,40 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import {AiFillHome,AiOutlineSearch, AiFillMail, AiOutlineUser, AiFillFileAdd} from 'react-icons/ai';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  AiFillHome,
+  AiOutlineSearch,
+  AiFillMail,
+  AiOutlineUser,
+  AiFillFileAdd,
+} from "react-icons/ai";
 
-import './NavBar.css'
+import "./NavBar.css";
 
 const Navbar = () => {
-    return (
-        <div id='navbar'>
-            <div>
-                <NavLink exact to='/'>
-                    <h3><AiFillHome/> Home</h3>
-                </NavLink>
-            </div>
-            <div>
-                <NavLink exact to='/contact'>
-                    <h3><AiFillMail/> Contact</h3>
-                </NavLink>            
-            </div>
-            <div>
-                <NavLink exact to='/createClub'>
-                    <h3><AiFillFileAdd/> Create Club</h3>
-                </NavLink>           
-            </div>  
-            <div className='search'>                
-                <NavLink to={'#'}>  
-                    <input type="text" placeholder='Buscar' />                  
-                    <button><AiOutlineSearch/></button>                 
-                </NavLink>
-            </div> 
-            <div className='login'>
-                <NavLink exact to='/login'>
-                    <h3><AiOutlineUser/> Login</h3>
-                </NavLink> 
-            </div>
-        </div>
-    );
-}
+  return (
+    <div id="navbar">
+      <div>
+        <NavLink exact="true" to="/">
+          <h3>
+            <AiFillHome /> Home
+          </h3>
+        </NavLink>
+      </div>
+      <div>
+        <NavLink exact="true" to="/contact">
+          <h3>
+            <AiFillMail /> Contact
+          </h3>
+        </NavLink>
+      </div>
+      <div>
+        <NavLink exact="true" to="/createClub">
+          <h3>
+            <AiFillFileAdd /> Create Club
+          </h3>
+        </NavLink>
+      </div>
+      <div className="search">
+        <NavLink to="#">
+          <input type="text" placeholder="Buscar" />
+          <button>
+            <AiOutlineSearch />
+          </button>
+        </NavLink>
+      </div>
+      <div className="login">
+        <NavLink exact="true" to="/login">
+          <h3>
+            <AiOutlineUser /> Login
+          </h3>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
