c20cc20e67206e292e727e8e2c53fa72c2683ca1
EzeTP <tejada.eap@gmail.com>
2022-03-21 15:13:27 -0300

    arregle una cosita de los ordenamientos por nombre y despues agregue ordenamiento por score

--- client/src/redux/reducer/metodos/order.js
+++ client/src/redux/reducer/metodos/order.js
@@ -1,7 +1,7 @@
 export const order = (array, orderBy) => {
   if (orderBy === "asc") {
     array = array.slice().sort((a, b) => {
-      var nameA = a.name.toUpperCase();
+      /*  var nameA = a.name.toUpperCase();
       var nameB = b.name.toUpperCase();
       if (nameA < nameB) {
         return -1;
@@ -9,11 +9,12 @@ export const order = (array, orderBy) => {
       if (nameA > nameB) {
         return 1;
       }
-      return 0;
+      return 0; */
+      return a.name.localeCompare(b.name);
     });
   } else if (orderBy === "dsc") {
     array = array.slice().sort((a, b) => {
-      var nameA = a.name.toUpperCase();
+      /* var nameA = a.name.toUpperCase();
       var nameB = b.name.toUpperCase();
       if (nameA > nameB) {
         return -1;
@@ -21,10 +22,10 @@ export const order = (array, orderBy) => {
       if (nameA < nameB) {
         return 1;
       }
-      return 0;
+      return 0; */
+      return b.name.localeCompare(a.name);
     });
-
-  } else if (orderBy === "hp") {
+  } /* else if (orderBy === "hp") {
     array = array
       .map((a) => a.Fields)
       .map((f) => f.slice().sort((b, a) => a.price - b.price));
@@ -33,6 +34,26 @@ export const order = (array, orderBy) => {
       a.Fields.map((f) => f.slice().sort((b, a) => b.price - a.price))
     );
 
+  } */ else if (orderBy === "hp") {
+    array = array.slice().sort((a, b) => {
+      if (a.score > b.score) {
+        return -1;
+      }
+      if (b.score > a.score) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (orderBy === "lp") {
+    array = array.slice().sort((a, b) => {
+      if (a.score > b.score) {
+        return 1;
+      }
+      if (b.score > a.score) {
+        return -1;
+      }
+      return 0;
+    });
   }
 
   return array;
