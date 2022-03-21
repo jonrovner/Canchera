5a8bb9af5e9911026a536db821c274321485dc15
JavierMusso <javier.musso96@gmail.com>
2022-03-21 12:26:34 -0300

    preps for deploying client, and deletes unused component

--- client/src/redux/action/index.js
+++ client/src/redux/action/index.js
@@ -9,10 +9,9 @@ export const SET_USER = "SET_USER";
 export const ORDER_NAME_CLUBS = "ORDER_NAME_CLUBS";
 export const ORDER_PRICE_CLUBS = "ORDER_PRICE_CLUBS";
 
-
 export const get_all_clubes = () => async (dispatch) => {
   try {
-    let clubes = await axios.get(`http://localhost:3001/club`);
+    let clubes = await axios.get(`/club`);
     console.log(clubes.data);
     return dispatch({ type: GET_ALL_CLUBES, payload: clubes.data });
   } catch (error) {
@@ -22,7 +21,7 @@ export const get_all_clubes = () => async (dispatch) => {
 
 export const get_users_email = (email) => async (dispatch) => {
   try {
-    let user = await axios.get(`http://localhost:3001/user?email=${email}`);
+    let user = await axios.get(`/user?email=${email}`);
     return dispatch({ type: GET_USERS_EMAIL, payload: user.data });
   } catch (error) {
     console.log(error);
@@ -31,7 +30,7 @@ export const get_users_email = (email) => async (dispatch) => {
 
 export const post_users_signin = (data) => async (dispatch) => {
   try {
-    let user = await axios.post(`http://localhost:3001/signin`, data);
+    let user = await axios.post(`/signin`, data);
     return dispatch({ type: POST_USERS_SIGNIN, payload: user.data });
   } catch (error) {
     console.log(error);
@@ -41,7 +40,7 @@ export const post_users_signin = (data) => async (dispatch) => {
 export const post_users_owner = (data) => async () => {
   console.log("Data enviada:", data);
   try {
-    await axios.post("http://localhost:3001/signup/owner", data);
+    await axios.post("/signup/owner", data);
   } catch (e) {
     console.log(e);
   }
@@ -50,7 +49,7 @@ export const post_users_owner = (data) => async () => {
 export const post_users = (data) => async () => {
   console.log("Data enviada:", data);
   try {
-    await axios.post("http://localhost:3001/signup/user", data);
+    await axios.post("/signup/user", data);
   } catch (e) {
     console.log(e);
   }
@@ -59,7 +58,7 @@ export const post_users = (data) => async () => {
 export const post_users_google = (data) => async () => {
   console.log("Data enviada:", data);
   try {
-    await axios.post("http://localhost:3001/signup/singup/google", data);
+    await axios.post("/signup/singup/google", data);
   } catch (e) {
     console.log(e);
   }
@@ -78,12 +77,10 @@ export const set_user = (user) => async (dispatch) => {
   return dispatch({ type: SET_USER, payload: user });
 };
 
-
 export const order_name_clubs = (order) => {
   return { type: ORDER_NAME_CLUBS, payload: order };
 };
 
 export const order_price_clubs = (order) => {
   return { type: ORDER_PRICE_CLUBS, payload: order };
-
 };
