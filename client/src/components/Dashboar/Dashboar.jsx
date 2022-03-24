9cacbea40cfa6bbf7c404462ea74a6c9021228f0
Jose Manuel Puello Cortina <puelloj66@gmail.com>
2022-03-24 18:35:13 -0500

    componente dashboar

---
+++ client/src/components/Dashboar/Dashboar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+function Dashboar() {
+    return ( 
+        <div>
+            <h1>Dashboar</h1> 
+            <p>renderizando rol: </p>
+ 
+
+            
+        </div>
+    );
+}
+
+export default Dashboar;
