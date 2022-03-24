e3d3b866f451df4daff0fd07ee447f147e741f4f
Carlos Martin Juncos <prof.mcjuncos@gmail.com>
2022-03-24 15:19:22 -0300

    resetInfoMail (#136)
    * resetInfoMail
    
    * required

--- client/src/components/Email/Email.jsx
+++ client/src/components/Email/Email.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import {Link} from 'react-router-dom'
 import emailjs from '@emailjs/browser';
 import styles from './Form.module.css'
 
@@ -11,11 +12,13 @@ export default function ContactUs  () {
     emailjs.sendForm('service_biq34kj', 'template_u82bwka', form.current, 'QB_YGsFz8TqzByi-w')
       .then((result) => {          
         console.log(result.text);
-        alert('Mensaje enviado!')
+        
       }, (error) => {
           console.log(error.text);
           alert('Su mensaje no pudo enviarse')
       });
+      e.target.reset()
+      alert('Mensaje enviado!')
   };
 
   return (
@@ -23,19 +26,26 @@ export default function ContactUs  () {
           <h1>Contactenos</h1>
     <form className={styles.formmail} ref={form} onSubmit={sendEmail}>
       <label>Nombre</label>
-      <input type="text" placeholder='Nombre...' name="user_name" />
+      <input type="text" placeholder='Nombre...' name="user_name" required />
 
       <label>Telefono</label>
-      <input type="tel" placeholder='telefono...' name="user_tel" />
+      <input type="tel" placeholder='telefono...' name="user_tel" required />
 
       <label>Email</label>
       <input type="email" placeholder='mail...' name="user_email" />
 
       <label>Mensaje</label>
-      <textarea type="text" placeholder='Su mensaje...' name="message" />
+      <textarea type="text" placeholder='Su mensaje...' name="message" required />
 
       <input className={styles.button} type="submit" value="Enviar" />
+    <br/>
+      <div> 
+      <Link to= '/'>
+        <button className={styles.button}>Volver</button>
+      </Link>
+    </div>
     </form>
+    
     </div>
   );
 };
