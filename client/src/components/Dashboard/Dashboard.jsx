30da1caa863cb1f43cfc8da393c29a188c614a9b
Jose Manuel Puello Cortina <43081221+JosePuelloCortina@users.noreply.github.com>
2022-03-24 22:15:16 -0500

    Dashboar2 (#150)
    * componente dashboar
    
    * componente dashboar
    
    * componente dashboar
    
    * componente dashboard
    
    * componente dashboard
    
    * componente dashboard
    
    * componente dashboard
    
    * componente dashboard 2
    
    Co-authored-by: Javier Musso <javier.musso96@gmail.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+
+import { useNavigate } from 'react-router-dom';
+
 import User from "./User";
 import Owner from "./Owner";
 import Admin from "./Admin";
+
+
+import Navbar from '../NavBar/NavBar';
 
 
 
 function Dashboard() {
 
     const user = useSelector(state => state.user)
-    
+
+
+    let navigate = useNavigate();
+
+    function onSubmit(e){
+        navigate('/login')
+    }
+   
     
     return ( 
         <div>
+            <Navbar/>
+
             <h1>Dashboard</h1> 
             {
                 user.rol === 'user' ? <User/> 
                 : user.rol === 'owner' ? <Owner/> 
                 : user.rol === 'admin' ? <Admin/> 
-                : <p>no hay usuario</p> 
+
+                :
+                <div>
+                    <p>no existe ningun usuario</p> 
+                    <button onClick={onSubmit}>Login</button>
+                </div>  
+
                 
             }
  
