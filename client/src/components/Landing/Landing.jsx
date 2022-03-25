7d4a2c0420fadc29b4effa5bc5bd41565f2677c9
Airton Nicolas Yañez <83186353+NicoYanez9621@users.noreply.github.com>
2022-03-25 11:33:19 -0300

    Reload page (#153)
    * Configuracion de back
    
    * reload a landing
    
    * reload a landing 2

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./Landing.module.css";
 import NavBar from "../NavBar/NavBar";
 import Slider from "../Slider/Slider";
@@ -7,13 +6,16 @@ import Footer from "../Footer/Footer";
 import heroImg from "../../assets/2.jpg";
 import playerImg from "../../assets/1.jpg";
 import hostImg from "../../assets/4.jpg";
-import { useSelector } from "react-redux";
 import Map from "../Map/Map";
 
 const Landing = () => {
-  let user = useSelector((state) => state.user);
-  //console.log("state: ", user);
-
+  if (window.localStorage) {
+    if (window.localStorage.getItem("reload")) {
+      localStorage.removeItem("reload");
+    } else {
+      window.localStorage.setItem("reload", true);
+    }
+  }
   return (
     <div className={styles.Landing}>
       <NavBar />
@@ -22,12 +24,13 @@ const Landing = () => {
           <h1>Decidete a jugar por mas!</h1>
           <img src={heroImg} alt="" />
         </section>
-        <h1>"Siempre quiero más. 
-            Ya sea que se trate de un gol o de ganar un juego, 
-            nunca estoy satisfecho"</h1>
-            <h2 >Messi</h2>
-            <br/>
-        <Slider />        
+        <h1>
+          "Siempre quiero más. Ya sea que se trate de un gol o de ganar un
+          juego, nunca estoy satisfecho"
+        </h1>
+        <h2>Messi</h2>
+        <br />
+        <Slider />
         <section className={styles.player}>
           <img src={playerImg} alt="" />
           <div>
@@ -38,14 +41,17 @@ const Landing = () => {
         <Collage />
         <section className={styles.host}>
           <div>
-            <h2>Busca y reserva en segundos tu cancha favorita, gratis y sin complicaciones. </h2>
+            <h2>
+              Busca y reserva en segundos tu cancha favorita, gratis y sin
+              complicaciones.{" "}
+            </h2>
             <button>Aplicar</button>
           </div>
           <img src={hostImg} alt="" />
         </section>
       </div>
-      <Map/>
-      <br/>
+      <Map />
+      <br />
       <Footer />
     </div>
   );
