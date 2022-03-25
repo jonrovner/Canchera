9abb6f3ea31685b16e6c00ac9238bd341d60e321
Jose Manuel Puello Cortina <43081221+JosePuelloCortina@users.noreply.github.com>
2022-03-24 20:37:40 -0500

    Dashboar (#149)
    * componente dashboar
    
    * componente dashboar
    
    * componente dashboar
    
    * componente dashboard
    
    * componente dashboard

---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import User from "./User";
+import Owner from "./Owner";
+import Admin from "./Admin";
+
+
+
+function Dashboard() {
+
+    const user = useSelector(state => state.user)
+    
+    
+    return ( 
+        <div>
+            <h1>Dashboard</h1> 
+            {
+                user.rol === 'user' ? <User/> 
+                : user.rol === 'owner' ? <Owner/> 
+                : user.rol === 'admin' ? <Admin/> 
+                : <p>no hay usuario</p> 
+                
+            }
+ 
+
+            
+        </div>
+    );
+}
+
+export default Dashboard;
