e57d8028cb622ef891ccd2b25395326841138436
Lucas <91353502+LucasZupan@users.noreply.github.com>
2022-03-26 07:50:01 -0300

    added users to the DB (#163)

--- api/index.ts
+++ api/index.ts
@@ -20,10 +20,55 @@
 
 const db = require("./src/db");
 import server from "./src/app";
+const bcrypt = require("bcrypt");
+const authConfig = require("./src/config/auth.js");
+const { User } = require('./src/db.ts');
+
+async function dataDB() {
+
+  let encryptedPasswordUser = bcrypt.hashSync(process.env.USERPASSWORD, +authConfig.rounds);
+  let encryptedPasswordOwner = bcrypt.hashSync(process.env.OWNERPASSWORD, +authConfig.rounds);
+  let encryptedPasswordAdmin = bcrypt.hashSync(process.env.ADMINPASSWORD, +authConfig.rounds);
+
+  const userDB = {
+    name: process.env.USER,
+    email: process.env.USEREMAIL,
+    password: encryptedPasswordUser,
+    rol: 'user',
+    status: true
+  }
+  const ownerDB = {
+    name: process.env.OWNER,
+    email: process.env.OWNEREMAIL,
+    password: encryptedPasswordOwner,
+    rol: 'owner',
+    status: true
+  }
+  const adminDB = {
+    name: process.env.ADMIN,
+    email: process.env.ADMINEMAIL,
+    password: encryptedPasswordAdmin,
+    rol: 'admin',
+    status: true
+  }
+
+  try{
+    await User.create(userDB);
+    await User.create(ownerDB);
+    await User.create(adminDB);
+  }catch(e){
+    console.log("Ya existen esos usuarios en la db"+e)
+    }
+}
 
 // Syncing all the models at once.
 
+
 db.sequelize.sync({ force: false }).then(() => {
+
+  dataDB();
+
+
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001");
   });
