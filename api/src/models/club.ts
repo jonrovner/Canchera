15c65d87b923853771a6898249c850f1ba585ffe
Lucas <91353502+LucasZupan@users.noreply.github.com>
2022-03-27 18:44:54 -0300

    cambio el model club (#185)

--- api/src/models/club.ts
+++ api/src/models/club.ts
@@ -4,15 +4,17 @@ import { Model, UUIDV4 } from "sequelize";
 interface ClubAttributes {
   name: string;
   description: string;
-  location: string;
+  city: string;
+  street: string;
+  num: number;
+  province: string;
   openHour: number;
   closeHour: number;
   image: string;
   score: string;
   latitude: number;
   longitude: number;
-  lowestPrice: number;
-  ciudad: string;
+  lowestPrice: number;  
 }
 
 module.exports = (sequelize: any, DataTypes: any) => {
@@ -24,15 +26,17 @@ module.exports = (sequelize: any, DataTypes: any) => {
      */
     name!: string;
     description!: string;
-    location!: string;
+    city!: string;
+    street!: string;
+    num!: number;
+    province!: string;
     openHour!: number;
     closeHour!: number;
     image!: string;
     score!: string;
     latitude!: number;
     longitude!: number;
-    lowestPrice!: number;
-    ciudad!: string;
+    lowestPrice!: number;    
 
     static associate(models: any) {
       // define association here
@@ -57,7 +61,19 @@ module.exports = (sequelize: any, DataTypes: any) => {
       description: {
         type: DataTypes.STRING(1500),
       },
-      location: {
+      street: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      num: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      province: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
         type: DataTypes.STRING,
         allowNull: false,
       },
@@ -85,9 +101,7 @@ module.exports = (sequelize: any, DataTypes: any) => {
       lowestPrice: {
         type: DataTypes.INTEGER,
       },
-      ciudad: {
-        type: DataTypes.STRING,
-      },
+      
     },
     {
       sequelize,
