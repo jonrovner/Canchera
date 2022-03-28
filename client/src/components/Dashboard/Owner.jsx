19703183fe8894ab4d86dcc077d652f7d8605864
JavierMusso <javier.musso96@gmail.com>
2022-03-28 14:35:08 -0300

    arregla estilos en tablas de dashboards

--- client/src/components/Dashboard/Owner.jsx
+++ client/src/components/Dashboard/Owner.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   setHours,
@@ -6,31 +6,29 @@ import {
   setSeconds,
   addDays,
   subDays,
-  isToday
- 
+  isToday,
 } from "date-fns";
 import "./style/owner.css";
 import FieldCalendar from "../ClubDetail/FieldCalendar/FieldCalendar";
 import { useNavigate } from "react-router";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import styles from "./Dashboard.module.css";
+
 function Owner({ id, name, email, rol }) {
-  
-  const navigate = useNavigate()
-  const [owner, setOwner] = useState({})
-  const [club, setClub] = useState({})
+  const navigate = useNavigate();
+  const [owner, setOwner] = useState({});
+  const [club, setClub] = useState({});
 
-  useEffect(()=>{
-      const getOwner = (email) =>{
-        axios.get(`/owner?email=${email}`)
-        .then( res => setOwner(res.data))
-      }
-      getOwner(email)
+  useEffect(() => {
+    const getOwner = (email) => {
+      axios.get(`/owner?email=${email}`).then((res) => setOwner(res.data));
+    };
+    getOwner(email);
+  }, [email]);
 
-  },[email])
-  
-  useEffect(()=>{    
-    setClub(owner.Club)
-  },[setClub, owner.Club])
+  useEffect(() => {
+    setClub(owner.Club);
+  }, [setClub, owner.Club]);
 
   const [selectedDates, setSelectedDates] = useState([]);
 
@@ -63,109 +61,95 @@ function Owner({ id, name, email, rol }) {
     );
     if (!existent) {
       setSelectedDates([...selectedDates, { time: date, field: fieldId }]);
-     
+
       e.target.classList.add("selected");
     } else {
       setSelectedDates([
         ...selectedDates.filter((d) => d.time.toString() !== date.toString()),
       ]);
-      
+
       e.target.classList.remove("selected");
     }
-
-  }
+  };
 
   const handleBlock = async () => {
-    const toPost = {userId: owner.id, dates: selectedDates}
-    
-   const post = await axios.post('/booking', toPost)
-   console.log('booking response: ',post.data)
-   if (post.data.length) window.location.reload()
-    
-  }
+    const toPost = { userId: owner.id, dates: selectedDates };
 
-  console.log('owner: ', owner)
-  console.log('club: ', club)
+    const post = await axios.post("/booking", toPost);
+    console.log("booking response: ", post.data);
+    if (post.data.length) window.location.reload();
+  };
+
+  console.log("owner: ", owner);
+  console.log("club: ", club);
 
   return (
     <div>
       <h1>Bienvenido {name}</h1>
-      
+
       <div>
         <NavLink to="/createClub">
-          <button > create club</button>
+          <button> create club</button>
         </NavLink>
       </div>
 
-        {
-          club && 
-      (<div>
-            
-            <h3>Datos de su club </h3>
-            <p>Nombre: {club.name}</p>
-            <p>Descripción: {club.description}</p>
-            <p>Dirección: {`${club.street} ${club.num} ${club.ciudad}`}</p>
-            <p>Horario: {`de ${club.openHour} a ${club.closeHour} hs`}</p>
+      {club && (
+        <div>
+          <h3>Datos de su club </h3>
+          <p>Nombre: {club.name}</p>
+          <p>Descripción: {club.description}</p>
+          <p>Dirección: {`${club.street} ${club.num} ${club.ciudad}`}</p>
+          <p>Horario: {`de ${club.openHour} a ${club.closeHour} hs`}</p>
 
           <button>editar</button>
-      </div>)
-        }
+        </div>
+      )}
 
       <div>
         <h1>Reservas owner</h1>
         {club && club.Fields && (
-            <table id="myTable">
-            <tr className="header">
+          <table id="myTable">
+            <tr className={styles.header}>
               <th>Nombre</th>
               <th>Cancha</th>
               <th>Precio</th>
               <th>Reservas</th>
-              
             </tr>
             {club.Fields.map((field) => (
-            
-            <tr>
-              <td>{field.ClubName}</td>
-              <td>{field.id}</td>
-              <td>{field.price}</td>
-              <td>
-                <ul>
-                {
-                field.Bookings.length && field.Bookings.map(booking => (
-                  <li>{booking.time.toString()}</li>
-                ))
-
-                }
-                </ul>
+              <tr>
+                <td>{field.ClubName}</td>
+                <td>{field.id}</td>
+                <td>{field.price}</td>
+                <td>
+                  <ul>
+                    {field.Bookings.length &&
+                      field.Bookings.map((booking) => (
+                        <li>{booking.time.toString()}</li>
+                      ))}
+                  </ul>
                 </td>
-              
-            </tr>
+              </tr>
             ))}
-
           </table>
-
-
-        )
-          
-          
-          }
+        )}
 
         <div className="calendarControls">
-            <div className="button" onClick={handlePrevDay}>
-              ⏪
-            </div>
-            <p>
-              {isToday(selectedDay) ? "hoy" : selectedDay.toLocaleDateString()}
-            </p>
-            <div className="button" onClick={handleNextDay}>
-              ⏩
-            </div>
+          <div className="button" onClick={handlePrevDay}>
+            ⏪
           </div>
+          <p>
+            {isToday(selectedDay) ? "hoy" : selectedDay.toLocaleDateString()}
+          </p>
+          <div className="button" onClick={handleNextDay}>
+            ⏩
+          </div>
+        </div>
 
-          {
-            club && club.Fields && club.Fields.map( field => (
-              <>
-                <FieldCalendar 
+        {club &&
+          club.Fields &&
+          club.Fields.map((field) => (
+            <>
+              <FieldCalendar
                 day={selectedDay}
                 close={club.closeHour}
                 open={club.openHour}
@@ -174,12 +158,12 @@ function Owner({ id, name, email, rol }) {
                 price={field.price}
                 handleClick={handleCalendar}
                 fieldId={field.id}
-                surface={field.surface}/>
-            
+                surface={field.surface}
+              />
+
               <button onClick={handleBlock}>bloquear</button>
-              </>
-            ))
-          }
+            </>
+          ))}
       </div>
 
       {/* <div>
