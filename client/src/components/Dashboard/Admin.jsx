34d859b487fc0dee7b200c4428802fc314addd06
Javier Musso <javier.musso96@gmail.com>
2022-03-28 12:28:02 -0300

    agrega estilos a dashboard admin, y arregla algunos detalles. Agrega selector en lugar de input text (#211)

--- client/src/components/Dashboard/Admin.jsx
+++ client/src/components/Dashboard/Admin.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { delete_user, get_all_user, update_user } from "../../redux/action";
+import { TiDelete } from "react-icons/ti";
+import styles from "./Dashboard.module.css";
 
 function Admin({ user }) {
   let dispatch = useDispatch();
@@ -17,7 +19,7 @@ function Admin({ user }) {
   const deleteUser = async (id) => {
     await dispatch(delete_user(id));
     window.location.reload();
-  }; 
+  };
 
   const updateUser = async (id) => {
     await dispatch(update_user(id, data));
@@ -26,28 +28,32 @@ function Admin({ user }) {
 
   const handlerInputChange = (e) => {
     var value = e.target.value;
+    console.log(e.target.value);
     setData({ rol: value });
   };
 
   return (
-    <div>
+    <div className={styles.Admin}>
       <h1>Bienvenido {user.name}</h1>
-      <div key={user.id}>
-        <h1>Datos</h1>
-        <p>Name: {user.name}</p>
-        <p>Email: {user.email}</p>
-        <p>Rol: {user.rol}</p>
+      <div className={styles.users}>
         <ul>
           {allUsers &&
             allUsers.map((c) => (
               <li key={c.id}>
-                Name: {c.name} - Rol: {c.rol}
-                <button onClick={() => deleteUser(c.id)}>delete</button>
-                <button onClick={() => updateUser(c.id)}>update rol</button>
-                <input
-                  onChange={(e) => handlerInputChange(e)}
-                  type="text"
-                  placeholder="nuevo rol"
+                Nombre: {c.name} - Rol: {c.rol}
+                <button onClick={() => updateUser(c.id)}>Cambiar rol</button>
+                <select
+                  name="newRol"
+                  defaultValue={c.rol}
+                  onChange={handlerInputChange}
+                >
+                  <option value="user">User</option>
+                  <option value="owner">Owner</option>
+                  <option value="admin">Admin</option>
+                </select>
+                <TiDelete
+                  className={styles.delete}
+                  onClick={() => deleteUser(c.id)}
                 />
               </li>
             ))}{" "}
--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import User from "./User";
 import Owner from "./Owner";
 import Admin from "./Admin";
 import { useSelector } from "react-redux";
+import styles from "./Dashboard.module.css";
 
 import Navbar from "../NavBar/NavBar";
 
@@ -10,34 +11,29 @@ function Dashboard() {
   const user = useSelector((state) => state.user);
 
   return (
-    <div>
+    <div className={styles.Dashboard}>
       <Navbar />
-      <h1>Dashboard</h1>
-      {user.rol === "user" ? (
-        <div>
+      <div className={styles.switch}>
+        {user.rol === "user" ? (
           <User
             id={user.id}
             name={user.name}
             email={user.email}
             rol={user.rol}
           />
-        </div>
-      ) : user.rol === "owner" ? (
-        <div>
+        ) : user.rol === "owner" ? (
           <Owner
             id={user.id}
             name={user.name}
             email={user.email}
             rol={user.rol}
           />
-        </div>
-      ) : user.rol === "admin" ? (
-        <Admin user={user} />
-      ) : (
-        <div>
+        ) : user.rol === "admin" ? (
+          <Admin user={user} />
+        ) : (
           <p>Cargando... </p>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
