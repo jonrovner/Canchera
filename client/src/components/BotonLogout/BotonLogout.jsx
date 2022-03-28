95cd095c247cebe4292977657765386a92959fc8
Airton Nicolas Yañez <83186353+NicoYanez9621@users.noreply.github.com>
2022-03-28 00:31:56 -0300

    List clubs map (#200)
    * Configuracion de back
    
    * traje nuevos cambios en Usercontroller
    
    * login y logout modificadas las navegaciones
    
    * guardo comita para probar
    
    * deje club.location coomo estaba de un principio

--- client/src/components/BotonLogout/BotonLogout.jsx
+++ client/src/components/BotonLogout/BotonLogout.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
 import { clear_state_user } from "../../redux/action";
 import style from "./BotonLogout.module.sass";
 
 const BotonLogout = () => {
+  let navigate = useNavigate();
   let dispatch = useDispatch();
   const handleLogout = async () => {
     let desconectandose = localStorage.getItem("user");
     const desconectado = JSON.parse(desconectandose);
     dispatch(clear_state_user(desconectado.email));
-    console.log(desconectado.email);
     window.localStorage.clear();
+    navigate("/");
   };
   return (
     <div>
