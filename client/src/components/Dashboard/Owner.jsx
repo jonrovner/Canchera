8cb84821bf0be50372b081935a8d3e4e7542f0ac
Airton Nicolas Yañez <83186353+NicoYanez9621@users.noreply.github.com>
2022-03-28 01:03:24 -0300

    Admin arreglado (#202)
    * Configuracion de back
    
    * traje nuevos cambios en Usercontroller
    
    * arregle lo que le paso a admin en dashboard
    
    * arreglado bug de redireccion a dashboard de owner

--- client/src/components/Dashboard/Owner.jsx
+++ client/src/components/Dashboard/Owner.jsx
@@ -1,51 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import './style/owner.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import "./style/owner.css";
 
-function Owner({id, name, email, rol}) {
+function Owner({ id, name, email, rol }) {
+  const user = useSelector((state) => state.user);
+  const clubes = useSelector((state) => state.clubDetail);
 
-    const user = useSelector(state => state.user)
-    const clubes = useSelector(state => state.clubDetail)
-    
-    let boo = user.Bookings;
+  let boo = user.Bookings;
 
-        return (
-            <div> 
-                <h1>Bienvenido {name}</h1> 
-                <div key={id}>
-                    <h1>Datos</h1>
-                    <p>Name: {name}</p>
-                    <p>Email: {email}</p>
-                    <p>Rol: {rol}</p>
-                </div>
-                <div>
-                    <a href="/createClub"> create club</a>
-                </div>
-                <div>
-                        <h1>Reservas owner</h1>
-                    {
-                        boo.length && boo.map((b) =>(   
-                            <table id="myTable">
-                                <tr className="header">
-                                    <th >Club Name</th>
-                                    <th >Location</th>
-                                    <th >Time</th>
-                                    <th >Price</th>
-                                    <th >Surface</th>
-                                </tr>
-                                <tr>
-                                    <td>{b.Field.ClubName}</td>
-                                    <td>{b.Field.Club.location}</td>
-                                    <td>{b.time}</td>
-                                    <td>{b.Field.price}</td>
-                                    <td>{b.Field.surface}</td>
-                                </tr>
-                                </table>
-                        ))
-                    }
-                    </div> 
+  return (
+    <div>
+      <h1>Bienvenido {name}</h1>
+      <div key={id}>
+        <h1>Datos</h1>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Rol: {rol}</p>
+      </div>
+      <div>
+        <a href="/createClub"> create club</a>
+      </div>
+      <div>
+        <h1>Reservas owner</h1>
+        {boo &&
+          boo.map((b) => (
+            <table id="myTable">
+              <tr className="header">
+                <th>Club Name</th>
+                <th>Location</th>
+                <th>Time</th>
+                <th>Price</th>
+                <th>Surface</th>
+              </tr>
+              <tr>
+                <td>{b.Field.ClubName}</td>
+                <td>{b.Field.Club.location}</td>
+                <td>{b.time}</td>
+                <td>{b.Field.price}</td>
+                <td>{b.Field.surface}</td>
+              </tr>
+            </table>
+          ))}
+      </div>
 
-                    {/* <div>
+      {/* <div>
                      <h1>Clubes</h1>
                     {
                         clubes.length && clubes.map((o) =>(   
@@ -78,11 +76,9 @@ function Owner({id, name, email, rol}) {
                                 </table>
                         ))
                     }
-                    </div> */} 
-
-                    
-            </div>
-        );
+                    </div> */}
+    </div>
+  );
 }
 
 export default Owner;
