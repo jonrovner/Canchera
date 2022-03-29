52d9bbe7ed19d27b98a0f6584f7c7e70338f7bbc
leo10-kz <88455824+leo10-kz@users.noreply.github.com>
2022-03-29 23:26:37 +0200

    update club (#230)

--- api/src/routes/club.ts
+++ api/src/routes/club.ts
@@ -5,5 +5,6 @@ const ClubController = require("../controller/ClubController");
 router.post("/club", ClubController.uploadImage, ClubController.postClub);
 router.get("/club", ClubController.getClubs);
 router.get("/club/:clubName", ClubController.clubDetail);
+router.put("/club/:clubName", ClubController.updateClub);
 
 module.exports = router;
