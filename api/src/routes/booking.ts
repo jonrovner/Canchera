a885bc8330da8f96dc7b396b3a5732f681534fa2
leo10-kz <88455824+leo10-kz@users.noreply.github.com>
2022-03-31 17:13:32 +0200

    hice algunos cambios , la ruta del  plus necesita un poco mas (#238)

--- api/src/routes/booking.ts
+++ api/src/routes/booking.ts
@@ -6,6 +6,8 @@ router.post('/booking', BookingController.postBooking);
 
 router.get('/booking/:clubName', BookingController.getBookings);
 
-router.delete('/booking/:userId', BookingController.deleteBooking )
+router.delete('/booking/:userId', BookingController.deleteBooking );
+
+router.post('/booking/sendEmial/:id', BookingController.bookingInvitation);
 
 module.exports = router;
