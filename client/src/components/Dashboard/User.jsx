6eb7f226eea23177d77f201b6aab30834a4a8114
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2022-03-31 11:14:04 -0300

    implementacion de modal

--- client/src/components/Dashboard/User.jsx
+++ client/src/components/Dashboard/User.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import Modal from "./ModalInvitacion/Modal";
 import styles from "./Dashboard.module.css";
 
-function User({ name }) {
+function User({ id, name }) {
   const user = useSelector((state) => state.user);
   let boo = user.Bookings;
+
+  const [openModal, setOpenModal] = useState({
+    modal: false,
+    name: "",
+    id: "",
+  });
+
   return (
     <div>
       <h1>Bienvenido {name}</h1>
@@ -17,6 +25,7 @@ function User({ name }) {
             <th>Time</th>
             <th>Price</th>
             <th>Surface</th>
+            <th>Sharpe</th>
           </tr>
           {user &&
             boo.length &&
@@ -27,8 +36,24 @@ function User({ name }) {
                 <td>{b.time}</td>
                 <td>{b.Field.price}</td>
                 <td>{b.Field.surface}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      setOpenModal({ modal: true, name: name, id: id });
+                    }}
+                  >
+                    Sharpe
+                  </button>
+                </td>
               </tr>
             ))}
+          {openModal && (
+            <Modal
+              id={openModal.id}
+              name={openModal.name}
+              closeModal={setOpenModal}
+            />
+          )}
         </table>
       </div>
     </div>
