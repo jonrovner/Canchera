43e272397cbebf0f1ee27e2f8e8053cafd8c8325
Jose Manuel Puello Cortina <43081221+JosePuelloCortina@users.noreply.github.com>
2022-03-31 22:52:07 -0500

    loader (#247)

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./loader.css";
+
+function Loader() {
+    return (
+        <div id="contenedor">
+                    <div class="loader" id="loader">Loading...</div>
+        </div>
+        
+    );
+}
+
+export default Loader;
