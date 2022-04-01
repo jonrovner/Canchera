746877e699005a91a2e783a079c37a5b351e8cd4
jonrovner <jonrovner@gmail.com>
2022-04-01 08:42:27 -0300

    mas debuggin

--- client/src/components/ListClubs/ListClubs.jsx
+++ client/src/components/ListClubs/ListClubs.jsx
@@ -8,6 +8,11 @@ import OrderCiudad from "../Order/OrderCiudad";
 import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
 
 const ListClubs = () => {
+  let dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(get_all_clubes());
+  }, [dispatch]);
 
   const [filterClubs, setFilterClubs] = useState([])
   let clubes = useSelector((state) => state.clubes);
@@ -28,11 +33,7 @@ const ListClubs = () => {
     setActiveMarker(marker);
   };
 
-  let dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(get_all_clubes());
-  }, [dispatch]);
+  
 
   var clubXciudad = [];
   clubXciudad = clubes.filter((club) => club.ciudad === ciudad);
