8aa76316c74ab12f328024cd3a3a5331dc31dec6
Jonathan Rovner <jonrovner@gmail.com>
2022-04-03 15:52:06 -0300

    fix user dash refresh (#269)

--- client/src/components/Dashboard/User.jsx
+++ client/src/components/Dashboard/User.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Modal from "./ModalInvitacion/Modal";
 import styles from "./Dashboard.module.css";
+import axios from "axios";
 
 function User({ id, name }) {
-  const user = useSelector((state) => state.user);
-  let boo = user.Bookings;
-  console.log("boo: ", boo);
-  boo = boo.map((b) => {
-    return {
-      id: b.id,
-      ClubName: b.Field.ClubName,
-      time: new Date(b.time),
-      street: b.Field.Club.street,
-      num: b.Field.Club.num,
-      ciudad: b.Field.Club.ciudad,
-      price: b.Field.price,
-      surface: b.Field.surface,
-    };
-  });
+  const userMail = useSelector((state) => state.user).email;
+  
+  const [bookings, setBookings] = useState([]) 
+  const [user, setUser] = useState({})
+  
+  useEffect(()=>{
+    axios.get(`/user?email=${userMail}`)
+     .then( res => setUser(res.data))
+  }, [userMail])
+
+  useEffect(() => {
+    if (user.name) {
+      if(user.Bookings.length){
+        setBookings(user.Bookings.map((b) => {
+          return {
+            id: b.id,
+            ClubName: b.Field.ClubName,
+            time: new Date(b.time),
+            street: b.Field.Club.street,
+            num: b.Field.Club.num,
+            ciudad: b.Field.Club.ciudad,
+            price: b.Field.price,
+            surface: b.Field.surface,
+          };
+        }))    
+      }  
+    }    
+  }, [user]);
+ 
   const [openModal, setOpenModal] = useState({
     modal: false,
     name: "",
@@ -29,7 +44,8 @@ function User({ id, name }) {
     window.location.reload();
   };
 
-  console.log("boo: ", boo);
+  console.log("user: ", user);
+  console.log("boo: ", bookings);
   return (
     <div>
       <h1>Bienvenido {name}</h1>
@@ -53,8 +69,8 @@ function User({ id, name }) {
             <th>Invitar</th>
           </tr>
           {user &&
-            boo &&
-            boo.map((b) => (
+            bookings &&
+            bookings.map((b) => (
               <tr>
                 <td>{b.ClubName}</td>
                 <td>{b.street + " " + b.num + ", " + b.ciudad}</td>
