794289edc62fcb1c1ee7fd2b0d6a041b2277e265
Jonathan Rovner <jonrovner@gmail.com>
2022-04-16 20:52:24 -0300

    fixed club list (#329)

--- client/src/components/ListClubs/ListClubs.jsx
+++ client/src/components/ListClubs/ListClubs.jsx
@@ -1,52 +1,79 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ListClubs.module.sass";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  get_all_clubes,
-  locationFilter,
-  order_rating_clubs,
-} from "../../redux/action";
+import axios from "axios";
 import CardClub from "../CardClub/CardClub";
-import OrderName from "../Order/OrderName";
-import OrderCiudad from "../Order/OrderCiudad";
 import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
 import ScrollButton from "../ScrollButton/ScrollButton";
 import NavBar from "../NavBar/NavBarSinSearch";
 import Footer from "../Footer/FooterNoVideo";
 import { AiOutlineSearch } from "react-icons/ai";
 import { ImCross } from "react-icons/im";
-import { cities } from "../createClub/ar";
 import { BsArrowDownCircleFill, BsArrowUpCircleFill } from "react-icons/bs";
-import Loader from "../Loader/Loader";
-import { Link } from "react-router-dom";
+
 
 const ListClubs = () => {
-  let dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(get_all_clubes());
-  }, [dispatch]);
 
-  let serchBarResult = useSelector((state) => state.filterClubs);
-
-  let clubes = useSelector((state) => state.clubes);
-  const [filterClubs, setFilterClubs] = useState([]);
-
-  let intialClubes = clubes.map((club) => ({
-    ...club,
-    pos: { lat: club.latitude, lng: club.longitude },
-  }));
-
+  const [clubs, setClubs] = useState([])
+  const [filterClubs, setFilterClubs] = useState([])
+  const [cities, setCities] = useState([])
   const [activeMarker, setActiveMarker] = useState(null);
   const [mapPos, setMapPos] = useState({ lat: -32.9632, lng: -61.409 });
-  const [ciudad, setCiudad] = useState("Goya");
-  const [zoom, setZoom] = useState(4);
+  const [zoom, setZoom] = useState(4);  
   const [input, setInput] = useState({
     ciudad: "",
     size: "",
     clubName: "",
   });
-  const [showCities, setShowCities] = useState(false);
-  const [filterCities, setFilterCities] = useState([]);
+  
+  useEffect(() => {
+    const getClubs = async () =>{
+      let res = await axios.get(`/club`)
+      setClubs(res.data.map((club) => ({
+        ...club,
+        pos: { lat: club.latitude, lng: club.longitude },
+      })))
+
+    }
+    getClubs()
+  }, []);
+
+  useEffect(()=> {
+    setFilterClubs(clubs)
+    setCities([...new Set(clubs.map(c => c.ciudad))])
+  },[clubs])
+        
+  
+
+  const handleFilter = () => {
+    let { ciudad, size, clubName } = input;
+    let newClubes = filterClubs.length && [...filterClubs];
+    // filtro por ciudad, si es que existe.    
+    if (ciudad) {
+      newClubes = newClubes.filter((club) =>
+        club.ciudad.toUpperCase().includes(ciudad.toUpperCase())
+      );     
+      setMapPos(newClubes[0].pos)
+      setZoom(12)       
+    }    
+    // filtro por size, si es que existe.
+    if (size) {
+      newClubes = newClubes.filter((club) => {
+        let test = club.Fields.map((field) => {
+          return field.players;
+        });
+        return test.includes(+size);
+      });
+    }  
+    // filtro por clubName si es que existe.
+    if (clubName) {
+      newClubes = newClubes.filter((club) =>
+        club.name.toUpperCase().includes(clubName.toUpperCase())
+      );
+    }  
+    // devuelvo lo que haya quedado en clubes.
+    setFilterClubs(newClubes);
+  };
+
 
   const handleActiveMarker = (marker) => {
     if (marker === activeMarker) {
@@ -55,88 +82,40 @@ const ListClubs = () => {
     setActiveMarker(marker);
   };
 
-  var clubXciudad = [];
-  clubXciudad = clubes.filter((club) => club.ciudad === ciudad);
-
-  const positions =
-    clubes &&
-    clubes.map((club) => ({ lat: club.latitude, lng: club.longitude }));
-
-  const handleSelect = (e) => {
-    setCiudad(e.target.value);
-    clubXciudad = clubes.filter((club) => club.ciudad === e.target.value);
-
-    if (clubXciudad.length) {
-      let lat = clubXciudad[0].latitude;
-      let lng = clubXciudad[0].longitude;
-      let newPos = { lat: lat, lng: lng };
-      setMapPos(newPos);
-      setZoom(14);
-    } else {
-      setMapPos({ lat: -32.9632, lng: -61.409 });
-      setZoom(4);
-    }
-  };
-
   const [mapFilter, setMapFilter] = useState(false);
-
   const handleMapFilter = () => {
     setMapFilter(!mapFilter);
-    dispatch(locationFilter());
-    serchBarResult = [];
-    setInput({
-      ciudad: "",
-      size: "",
-      clubName: "",
-    });
-    setMapPos({ lat: -32.9632, lng: -61.409 });
-    setZoom(4);
   };
 
-  const [mapBounds, setMapBounds] = useState({});
-  //console.log('map bounds: ', mapBounds)
+  const [mapBounds, setMapBounds] = useState({
+    "Ab": {
+      "h": -53.599458918451724,
+      "j": -6.152205029798327
+    },
+    "Ua": {
+      "h": -80.393375,
+      "j": -42.424625000000006
+    }
+  });
+  
+  
   useEffect(() => {
-    if (clubes.length ) {
-      let filtered = intialClubes.filter((club) =>
-        mapBounds.contains(club.pos)
+    if (clubs.length && mapFilter) {
+      let filtered = clubs.filter((club) =>
+        mapBounds.contains && mapBounds.contains(club.pos)
       );
-      console.log('filtering clubs')
       setFilterClubs(filtered);
     }
-  }, [mapBounds]);
+  }, [mapBounds, clubs.length]);
 
-  useEffect(() => {
-    if (
-      (!input.ciudad && !input.size && !input.clubName) ||
-      (input.size && !input.ciudad) ||
-      (input.clubName && serchBarResult.length > 1)
-    ) {
-      setMapPos({ lat: -32.9632, lng: -61.409 });
-      setZoom(4);
-    } else if (serchBarResult.length) {
-      setMapPos({
-        lat: serchBarResult[0].latitude,
-        lng: serchBarResult[0].longitude,
-      });
-      setZoom(12);
-    } else {
-      setMapPos({ lat: -32.9632, lng: -61.409 });
-      setZoom(4);
-    }
-    console.log(serchBarResult);
-  }, [serchBarResult]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    // aca ya esta listo para ir a /clubs y filtrar segun lo pedido.
-    // await dispatch(get_all_clubes());
-
-    if (!input.ciudad && !input.size && !input.clubName) {
+    e.preventDefault();   
+    if (!input.ciudad) {
       setMapPos({ lat: -32.9632, lng: -61.409 });
-      setZoom(4);
-      await dispatch(locationFilter(input));
+      setZoom(4);      
     } else {
-      await dispatch(locationFilter(input));
+      handleFilter()
     }
   };
 
@@ -144,30 +123,6 @@ const ListClubs = () => {
     e.preventDefault();
     setInput({ ...input, [e.target.name]: e.target.value });
   };
-
-  useEffect(() => {
-    if (input.ciudad && input.ciudad.length > 0) {
-      setShowCities(true);
-    }
-    if (!input.ciudad || input.ciudad.length < 1) {
-      setShowCities(false);
-    }
-  }, [input.ciudad]);
-
-  useEffect(() => {
-    const findMatch = (word, cities) => {
-      const regex = new RegExp(word, "gi");
-
-      setFilterCities(
-        cities.filter((place) => {
-          return place.city.match(regex);
-        })
-      );
-    };
-    if (input.ciudad && input.ciudad.length) {
-      findMatch(input.ciudad, cities);
-    }
-  }, [input.ciudad]);
 
   const defaultMapOptions = {
     fullscreenControl: false,
@@ -184,33 +139,22 @@ const ListClubs = () => {
     ],
   };
 
-  const hadlerHigherRating = () => {
-    let order = "hr";
-    //alert("mayor calificacion");
-    dispatch(order_rating_clubs(order));
-    setFilterClubs(filterClubs.sort((a, b) => b.score - a.score));
-  };
-
-  const hadlerLowerRating = () => {
-    let order = "lr";
-    //alert("menor calificacion");
-    dispatch(order_rating_clubs(order));
-    setFilterClubs(filterClubs.sort((a, b) => a.score - b.score));
-  };
-
   const vaciarInput = async () => {
     setInput({
       ciudad: "",
       size: "",
       clubName: "",
     });
+    setFilterClubs(clubs)
     setMapPos({ lat: -32.9632, lng: -61.409 });
     setZoom(4);
-    await dispatch(locationFilter());
   };
-  console.log("searchbarresult: ", serchBarResult);
-  console.log("clubes: ", clubes);
+
+  console.log("clubes: ", clubs);
+  console.log('cities: ', cities);
   console.log("filterClubs: ", filterClubs);
+  
+  
   return (
     <div className={styles.ListClubs}>
       <div className={styles.nonFooter}>
@@ -220,10 +164,15 @@ const ListClubs = () => {
             <div className={styles.orderScore}>
               Ordenar por Calificación
               <div>
-                <button onClick={hadlerHigherRating}>
+                <button onClick={() => {
+                  let clubes = [...filterClubs.sort((a,b) => b.score-a.score)]
+                  setFilterClubs(clubes)}}>
                   <BsArrowUpCircleFill />
                 </button>
-                <button onClick={hadlerLowerRating}>
+                <button onClick={()=>{
+                  let clubes = [...filterClubs.sort((a,b)=> a.score-b.score)]
+                  setFilterClubs(clubes)
+                }}>
                   <BsArrowDownCircleFill />
                 </button>
               </div>
@@ -247,11 +196,9 @@ const ListClubs = () => {
                 />
                 {
                   <datalist id="cityname">
-                    {filterCities.length &&
-                      showCities &&
-                      filterCities
-                        .slice(0, 10)
-                        .map((city, i) => <option key={i} value={city.city} />)}
+                    {cities.length &&
+                      cities
+                        .map((city, i) => <option key={i} value={city} />)}
                   </datalist>
                 }
                 <input
@@ -279,47 +226,7 @@ const ListClubs = () => {
 
           <div className={styles.clubesYmap}>
             <div className={styles.clubes}>
-              {serchBarResult.length ? (
-                serchBarResult.map((c, i) => (
-                  <CardClub
-                    key={i}
-                    name={c.name}
-                    img={c.image}
-                    location={
-                      c.street + " " + c.num + " " + c.ciudad + " " + c.province
-                    }
-                    openHour={c.openHour}
-                    closeHour={c.closeHour}
-                    Fields={c.Fields}
-                    score={c.score}
-                  />
-                ))
-              ) : !mapFilter ? (
-                clubes.length ? (
-                  clubes.map((c, i) => (
-                    <CardClub
-                      key={i}
-                      name={c.name}
-                      img={c.image}
-                      location={
-                        c.street +
-                        " " +
-                        c.num +
-                        " " +
-                        c.ciudad +
-                        " " +
-                        c.province
-                      }
-                      openHour={c.openHour}
-                      closeHour={c.closeHour}
-                      Fields={c.Fields}
-                      score={c.score}
-                    />
-                  ))
-                ) : (
-                  <Loader />
-                )
-              ) : filterClubs.length ? (
+              {filterClubs.length && (
                 filterClubs.map((c, i) => (
                   <CardClub
                     key={i}
@@ -334,10 +241,9 @@ const ListClubs = () => {
                     score={c.score}
                   />
                 ))
-              ) : (
-                <Loader />
-              )}
+              ) }
             </div>
+            
             <div className={styles.map}>
               <label className={styles.mapFilter} htmlFor="mapFilter">
                 Usar mapa como filtro
@@ -365,14 +271,11 @@ const ListClubs = () => {
                   });
                 }}
               >
-                {serchBarResult.length
-                  ? serchBarResult.map((club, index) => (
+                {filterClubs.length
+                  && filterClubs.map((club, index) => (
                       <Marker
                         key={index}
-                        position={{
-                          lat: club.latitude,
-                          lng: club.longitude,
-                        }}
+                        position={club.pos}
                         icon={{
                           url: "https://i.postimg.cc/wjKd121N/mark-Canchera.png",
                         }}
@@ -386,36 +289,6 @@ const ListClubs = () => {
                               '{club.name}'
                               <br />
                               {club.location}
-                            </div>
-                          </InfoWindow>
-                        ) : null}
-                      </Marker>
-                    ))
-                  : clubes.map((club, index) => (
-                      <Marker
-                        key={index}
-                        position={{
-                          lat: club.latitude,
-                          lng: club.longitude,
-                        }}
-                        icon={{
-                          url: "https://i.postimg.cc/wjKd121N/mark-Canchera.png",
-                        }}
-                        onClick={() => handleActiveMarker(club.name)}
-                      >
-                        {activeMarker === club.name ? (
-                          <InfoWindow
-                            onCloseClick={() => setActiveMarker(null)}
-                          >
-                            <div>
-                              <p>
-                                {club.name}, {club.score} ⭐<br />
-                                <Link
-                                  to={`/club/${club.name.replaceAll(" ", "-")}`}
-                                >
-                                  Reservar Aqui
-                                </Link>
-                              </p>
                             </div>
                           </InfoWindow>
                         ) : null}
